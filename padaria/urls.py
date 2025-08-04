1a13ed5605af0b489a1688e5ca08d2c2b5f8a506
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-08-04 15:28:32 -0300

    URL alterada

--- padaria/urls.py
+++ padaria/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('padaria_app.urls')),
+    path('padaria/', include('padaria_app.urls')),
 ]
